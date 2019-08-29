66ae98a9b029c9925ae4beb7e851cd6a1b5ca6a7
taniabarreda <51935510+taniabarreda@users.noreply.github.com>
2019-08-29 08:54:45 -0500

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,6 +1,16 @@
-
-
+#include <iostream>
+using namespace std;
 int main() {
-
+  int lado;
+  cin>>lado;
+    for (int i=0;i<=lado;i++){
+      for(int j=0;j<=lado;j++){
+        if (i==0||j==0||i==lado||j==lado)
+        cout<<"*";
+        else
+        cout<<" ";
+      }
+      cout<<endl;
+    }
     return 0;
 }
